361576c21161c94ac5e97fe1c5eafcdd71cab4bd
Adrian Pluis <adriancpluis@icloud.com>
2023-09-22 11:17:22 +1000

    100 DOC

---
+++ 06_rollercoaster_ride.py
@@ -0,0 +1,24 @@
+print("Welcome to the rollercoaster ride!")
+print("You must be 120cm or taller to ride the rollercoaster.")
+height = int(input("How tall are you (cm)?"))
+bill = 0
+
+if height >= 120:
+    age = int(input("How old are you? "))
+    if age >= 18:
+        bill += 8
+        print("Hop aboard the rollercoaster! One adult ticket will be $8")
+    elif age < 18 and age > 12:
+        bill += 7
+        print("Hop aboard the rollercoaster! One adult ticket will be $7")
+    else:
+        bill += 6
+        print("Hop aboard the rollercoaster! One adult ticket will be $6")
+
+    wants_photo = input("Do you want to have a photo taken? (Y/N)")
+    if wants_photo == "Y":
+        bill += 3
+
+    print(f"Your final bill is {bill}")
+else:
+    print("Sorry, you are not tall enough to ride.")
