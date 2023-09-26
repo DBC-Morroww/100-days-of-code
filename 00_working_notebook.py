eff7547665ab9e70810e5f3f7358100404539a3e
Adrian Pluis <adriancpluis@icloud.com>
2023-09-26 16:32:28 +1000

    push

--- 00_working_notebook.py
+++ 00_working_notebook.py
@@ -248,10 +248,39 @@ scissors = '''
       (____)
 ---.__(___)
 '''
+import random
 
-user_choice = (input("Do you want to choose rock, paper or scissors? \n")).lower()
+user_choice = int((input("Do you want to choose rock, paper or scissors? Select 0 for rock, 1 for paper, or 2 for scissors: \n")).lower())
 
-print(f"you chose {user_choice}")
+if user_choice == 0:
+    print(rock)
+    print("You selected 'rock'")
+elif user_choice == 1:
+    print(paper)
+    print("You selected 'paper'")
+elif user_choice == 2:
+    print(scissors)
+    print("You selected 'scissors'")
 
-print(type(user_choice))
+computer_choice = random.randint(0,2)
 
+if computer_choice == 0:
+    print(rock)
+    print("Computer selected 'rock'")
+elif computer_choice == 1:
+    print(paper)
+    print("Computer selected 'paper'")
+elif computer_choice == 2:
+    print(scissors)
+    print("Computer selected 'scissors'")
+
+if computer_choice == 0 and user_choice == 2:
+    print("Computer wins, you lose!")
+elif user_choice == 0 and computer_choice == 2:
+    print("You win!")
+elif user_choice > computer_choice:
+    print("You win!")
+elif computer_choice > user_choice:
+    print("Computer wins, you lose!")
+elif user_choice == computer_choice:
+    print("It's a draw!")
